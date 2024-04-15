369c0cf98a8416cfb285b401a2c25e7eda3f3764
vaibhav2169 <vb537718@dal.ca>
2024-04-15 00:04:19 -0300

    Committing changes

--- settings.py
+++ settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-ROOT_URLCONF = 'RBC.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'RBC.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
