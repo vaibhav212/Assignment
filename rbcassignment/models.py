df27f3467288d5cbe2c7014e2aefd8da6fa165c8
vaibhav2169 <vb537718@dal.ca>
2024-04-23 09:15:24 -0300

    commit

--- rbcassignment/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Message(models.Model):
-    id = models.AutoField(primary_key=True)
-    userID = models.CharField(max_length=100)
-    message = models.TextField()
--- urls.py
+++ urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from rbcassignment import views
+from backend import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
